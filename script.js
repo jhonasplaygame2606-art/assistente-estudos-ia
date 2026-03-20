a8536de6e66fec1054a69f0112e8a1630a289dc1
jhonasplaygame2606-art <jhonasplaygame2606@gmail.com>
2026-03-20 11:08:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,23 +14,26 @@ async function analisar() {
   respostaTela.innerText = "Analisando...";
 
   try {
-    let resposta = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        contents: [
-          {
-            parts: [
-              {
-                text: `Responda a pergunta abaixo e diga a alternativa correta:\n\n${pergunta}`
-              }
-            ]
-          }
-        ]
-      })
-    });
+    let resposta = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: `Responda a pergunta abaixo e diga a alternativa correta:\n\n${pergunta}`
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
 
     let dados = await resposta.json();
 
